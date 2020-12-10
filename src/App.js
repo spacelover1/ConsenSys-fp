3f24a3d7fb5ec1c551dc837cc7690dfccf85b830
spacelover1 <zeinabsalimi123@gmail.com>
2020-12-10 14:29:07 +0330

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import { MarketPlace } from './abi/abi.js'
+import Web3 from 'web3'
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Navbar'
+import Main from './Main'
+
+const web3 = new Web3(Web3.givenProvider);
+const contractAddress = "0x38514b09cc40C815dd4Ee6F5A4a24F7C3Ba5118A";
+const storageContract = new web3.eth.Contract(MarketPlace, contractAddress);
+
+class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    console.log(window.web3)
+    // await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  // async loadBlockchainData() {
+  //   const web3 = window.web3
+  //   // Load account
+  //   const accounts = await web3.eth.getAccounts()
+  //   this.setState({ account: accounts[0] })
+  //   const networkId = await web3.eth.net.getId()
+  //   const networkData = MarketPlace.networks[networkId]
+  //   if(networkData) {
+  //     const marketplace = web3.eth.Contract(MarketPlace.abi, networkData.address)
+  //     console.log(marketplace)
+  //   } else {
+  //     window.alert('MarketPlace contract not deployed to detected network.')
+  //   }
+  // }
+
+
+constructor(props) {
+  super(props)
+  this.state = {
+    account: '',
+    productCount: 0,
+    products: [],
+    loading: true
+  }
+
+  this.createProduct = this.createProduct.bind(this)
+  this.purchaseProduct = this.purchaseProduct.bind(this)
+}
+
+createProduct(name, price) {
+  this.setState({ loading: true })
+  this.state.marketplace.methods.createProduct(name, price).send({ from: this.state.account })
+  .once('receipt', (receipt) => {
+    this.setState({ loading: false })
+  })
+}
+
+purchaseProduct(id, price) {
+  this.setState({ loading: true })
+  this.state.marketplace.methods.purchaseProduct(id).send({ from: this.state.account, value: price })
+  .once('receipt', (receipt) => {
+    this.setState({ loading: false })
+  })
+}
+
+render() {
+  return (
+    <div>
+      <Navbar account={this.state.account} />
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <main role="main" className="col-lg-12 d-flex">
+            { this.state.loading
+              ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
+              : <Main
+                products={this.state.products}
+                createProduct={this.createProduct}
+                purchaseProduct={this.purchaseProduct} />
+            }
+          </main>
+        </div>
+      </div>
+    </div>
+  );
+}
+}
+
+export default App;
